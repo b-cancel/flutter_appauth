7a8f73c4e8dcfcc9325d0a516cfcffd0ff283b39
Bryan Cancel <bryan.o.cancel@gmail.com>
2023-04-23 16:35:26 -0400

    trailing commas in test before add new test before making implements

--- flutter_appauth_platform_interface/test/method_channel_flutter_appauth_test.dart
+++ flutter_appauth_platform_interface/test/method_channel_flutter_appauth_test.dart
@@ -5,12 +5,15 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  const MethodChannel channel =
-      MethodChannel('crossingthestreams.io/flutter_appauth');
+  const MethodChannel channel = MethodChannel(
+    'crossingthestreams.io/flutter_appauth',
+  );
   final List<MethodCall> log = <MethodCall>[];
-  channel.setMockMethodCallHandler((MethodCall methodCall) async {
-    log.add(methodCall);
-  });
+  channel.setMockMethodCallHandler(
+    (MethodCall methodCall) async {
+      log.add(methodCall);
+    },
+  );
 
   tearDown(() {
     log.clear();
@@ -19,171 +22,250 @@ void main() {
   final MethodChannelFlutterAppAuth flutterAppAuth =
       MethodChannelFlutterAppAuth();
 
-  test('authorize', () async {
-    await flutterAppAuth.authorize(AuthorizationRequest(
-        'someClientId', 'someRedirectUrl',
-        discoveryUrl: 'someDiscoveryUrl', loginHint: 'someLoginHint'));
-    expect(
-      log,
-      <Matcher>[
-        isMethodCall('authorize', arguments: <String, Object?>{
-          'clientId': 'someClientId',
-          'issuer': null,
-          'redirectUrl': 'someRedirectUrl',
-          'discoveryUrl': 'someDiscoveryUrl',
-          'loginHint': 'someLoginHint',
-          'scopes': null,
-          'serviceConfiguration': null,
-          'additionalParameters': null,
-          'allowInsecureConnections': false,
-          'preferEphemeralSession': false,
-          'promptValues': null,
-          'responseMode': null,
-          'nonce': null,
-        })
-      ],
-    );
-  });
-
-  test('authorizeAndExchangeCode', () async {
-    await flutterAppAuth.authorizeAndExchangeCode(AuthorizationTokenRequest(
-        'someClientId', 'someRedirectUrl',
-        discoveryUrl: 'someDiscoveryUrl',
-        loginHint: 'someLoginHint',
-        responseMode: 'fragment'));
-    expect(
-      log,
-      <Matcher>[
-        isMethodCall('authorizeAndExchangeCode', arguments: <String, Object?>{
-          'clientId': 'someClientId',
-          'issuer': null,
-          'redirectUrl': 'someRedirectUrl',
-          'discoveryUrl': 'someDiscoveryUrl',
-          'loginHint': 'someLoginHint',
-          'scopes': null,
-          'serviceConfiguration': null,
-          'additionalParameters': null,
-          'allowInsecureConnections': false,
-          'preferEphemeralSession': false,
-          'promptValues': null,
-          'clientSecret': null,
-          'refreshToken': null,
-          'authorizationCode': null,
-          'grantType': 'authorization_code',
-          'codeVerifier': null,
-          'responseMode': 'fragment',
-          'nonce': null,
-        })
-      ],
-    );
-  });
-
-  group('token', () {
-    test('cannot infer grant type', () async {
-      expect(
-          () async => await flutterAppAuth.token(TokenRequest(
-              'someClientId', 'someRedirectUrl',
-              discoveryUrl: 'someDiscoveryUrl')),
-          throwsArgumentError);
-    });
-    test('infers refresh token grant type', () async {
-      await flutterAppAuth.token(TokenRequest('someClientId', 'someRedirectUrl',
-          discoveryUrl: 'someDiscoveryUrl', refreshToken: 'someRefreshToken'));
-      expect(
-        log,
-        <Matcher>[
-          isMethodCall('token', arguments: <String, Object?>{
-            'clientId': 'someClientId',
-            'issuer': null,
-            'redirectUrl': 'someRedirectUrl',
-            'discoveryUrl': 'someDiscoveryUrl',
-            'scopes': null,
-            'serviceConfiguration': null,
-            'additionalParameters': null,
-            'allowInsecureConnections': false,
-            'clientSecret': null,
-            'refreshToken': 'someRefreshToken',
-            'authorizationCode': null,
-            'grantType': 'refresh_token',
-            'codeVerifier': null,
-            'nonce': null,
-          })
-        ],
-      );
-    });
-
-    test('infers authorization code grant type', () async {
-      await flutterAppAuth.token(TokenRequest('someClientId', 'someRedirectUrl',
+  test(
+    'authorize',
+    () async {
+      await flutterAppAuth.authorize(
+        AuthorizationRequest(
+          'someClientId',
+          'someRedirectUrl',
           discoveryUrl: 'someDiscoveryUrl',
-          authorizationCode: 'someAuthorizationCode'));
+          loginHint: 'someLoginHint',
+        ),
+      );
       expect(
         log,
         <Matcher>[
-          isMethodCall('token', arguments: <String, Object?>{
-            'clientId': 'someClientId',
-            'issuer': null,
-            'redirectUrl': 'someRedirectUrl',
-            'discoveryUrl': 'someDiscoveryUrl',
-            'scopes': null,
-            'serviceConfiguration': null,
-            'additionalParameters': null,
-            'allowInsecureConnections': false,
-            'clientSecret': null,
-            'refreshToken': null,
-            'authorizationCode': 'someAuthorizationCode',
-            'grantType': 'authorization_code',
-            'codeVerifier': null,
-            'nonce': null,
-          })
+          isMethodCall(
+            'authorize',
+            arguments: <String, Object?>{
+              'clientId': 'someClientId',
+              'issuer': null,
+              'redirectUrl': 'someRedirectUrl',
+              'discoveryUrl': 'someDiscoveryUrl',
+              'loginHint': 'someLoginHint',
+              'scopes': null,
+              'serviceConfiguration': null,
+              'additionalParameters': null,
+              'allowInsecureConnections': false,
+              'preferEphemeralSession': false,
+              'promptValues': null,
+              'responseMode': null,
+              'nonce': null,
+            },
+          ),
         ],
       );
-    });
+    },
+  );
 
-    test('sends specified grant type', () async {
-      await flutterAppAuth.token(TokenRequest('someClientId', 'someRedirectUrl',
-          discoveryUrl: 'someDiscoveryUrl', grantType: 'someGrantType'));
+  test(
+    'authorizeAndExchangeCode',
+    () async {
+      await flutterAppAuth.authorizeAndExchangeCode(
+        AuthorizationTokenRequest(
+          'someClientId',
+          'someRedirectUrl',
+          discoveryUrl: 'someDiscoveryUrl',
+          loginHint: 'someLoginHint',
+          responseMode: 'fragment',
+        ),
+      );
       expect(
         log,
         <Matcher>[
-          isMethodCall('token', arguments: <String, Object?>{
-            'clientId': 'someClientId',
-            'issuer': null,
-            'redirectUrl': 'someRedirectUrl',
-            'discoveryUrl': 'someDiscoveryUrl',
-            'scopes': null,
-            'serviceConfiguration': null,
-            'additionalParameters': null,
-            'allowInsecureConnections': false,
-            'clientSecret': null,
-            'refreshToken': null,
-            'authorizationCode': null,
-            'grantType': 'someGrantType',
-            'codeVerifier': null,
-            'nonce': null,
-          })
+          isMethodCall(
+            'authorizeAndExchangeCode',
+            arguments: <String, Object?>{
+              'clientId': 'someClientId',
+              'issuer': null,
+              'redirectUrl': 'someRedirectUrl',
+              'discoveryUrl': 'someDiscoveryUrl',
+              'loginHint': 'someLoginHint',
+              'scopes': null,
+              'serviceConfiguration': null,
+              'additionalParameters': null,
+              'allowInsecureConnections': false,
+              'preferEphemeralSession': false,
+              'promptValues': null,
+              'clientSecret': null,
+              'refreshToken': null,
+              'authorizationCode': null,
+              'grantType': 'authorization_code',
+              'codeVerifier': null,
+              'responseMode': 'fragment',
+              'nonce': null,
+            },
+          ),
         ],
       );
-    });
-  });
+    },
+  );
 
-  test('endSession', () async {
-    await flutterAppAuth.endSession(EndSessionRequest(
-        idTokenHint: 'someIdToken',
-        postLogoutRedirectUrl: 'somePostLogoutRedirectUrl',
-        state: 'someState',
-        discoveryUrl: 'someDiscoveryUrl'));
-    expect(log, <Matcher>[
-      isMethodCall('endSession', arguments: <String, Object?>{
-        'idTokenHint': 'someIdToken',
-        'postLogoutRedirectUrl': 'somePostLogoutRedirectUrl',
-        'state': 'someState',
-        'allowInsecureConnections': false,
-        'additionalParameters': null,
-        'issuer': null,
-        'discoveryUrl': 'someDiscoveryUrl',
-        'serviceConfiguration': null,
-        'preferEphemeralSession': false,
-      })
-    ]);
-  });
+  group(
+    'token',
+    () {
+      test(
+        'cannot infer grant type',
+        () async {
+          expect(
+            () async => await flutterAppAuth.token(
+              TokenRequest(
+                'someClientId',
+                'someRedirectUrl',
+                discoveryUrl: 'someDiscoveryUrl',
+              ),
+            ),
+            throwsArgumentError,
+          );
+        },
+      );
+      test(
+        'infers refresh token grant type',
+        () async {
+          await flutterAppAuth.token(
+            TokenRequest(
+              'someClientId',
+              'someRedirectUrl',
+              discoveryUrl: 'someDiscoveryUrl',
+              refreshToken: 'someRefreshToken',
+            ),
+          );
+          expect(
+            log,
+            <Matcher>[
+              isMethodCall(
+                'token',
+                arguments: <String, Object?>{
+                  'clientId': 'someClientId',
+                  'issuer': null,
+                  'redirectUrl': 'someRedirectUrl',
+                  'discoveryUrl': 'someDiscoveryUrl',
+                  'scopes': null,
+                  'serviceConfiguration': null,
+                  'additionalParameters': null,
+                  'allowInsecureConnections': false,
+                  'clientSecret': null,
+                  'refreshToken': 'someRefreshToken',
+                  'authorizationCode': null,
+                  'grantType': 'refresh_token',
+                  'codeVerifier': null,
+                  'nonce': null,
+                },
+              ),
+            ],
+          );
+        },
+      );
+
+      test(
+        'infers authorization code grant type',
+        () async {
+          await flutterAppAuth.token(
+            TokenRequest(
+              'someClientId',
+              'someRedirectUrl',
+              discoveryUrl: 'someDiscoveryUrl',
+              authorizationCode: 'someAuthorizationCode',
+            ),
+          );
+          expect(
+            log,
+            <Matcher>[
+              isMethodCall(
+                'token',
+                arguments: <String, Object?>{
+                  'clientId': 'someClientId',
+                  'issuer': null,
+                  'redirectUrl': 'someRedirectUrl',
+                  'discoveryUrl': 'someDiscoveryUrl',
+                  'scopes': null,
+                  'serviceConfiguration': null,
+                  'additionalParameters': null,
+                  'allowInsecureConnections': false,
+                  'clientSecret': null,
+                  'refreshToken': null,
+                  'authorizationCode': 'someAuthorizationCode',
+                  'grantType': 'authorization_code',
+                  'codeVerifier': null,
+                  'nonce': null,
+                },
+              ),
+            ],
+          );
+        },
+      );
+
+      test(
+        'sends specified grant type',
+        () async {
+          await flutterAppAuth.token(
+            TokenRequest(
+              'someClientId',
+              'someRedirectUrl',
+              discoveryUrl: 'someDiscoveryUrl',
+              grantType: 'someGrantType',
+            ),
+          );
+          expect(
+            log,
+            <Matcher>[
+              isMethodCall(
+                'token',
+                arguments: <String, Object?>{
+                  'clientId': 'someClientId',
+                  'issuer': null,
+                  'redirectUrl': 'someRedirectUrl',
+                  'discoveryUrl': 'someDiscoveryUrl',
+                  'scopes': null,
+                  'serviceConfiguration': null,
+                  'additionalParameters': null,
+                  'allowInsecureConnections': false,
+                  'clientSecret': null,
+                  'refreshToken': null,
+                  'authorizationCode': null,
+                  'grantType': 'someGrantType',
+                  'codeVerifier': null,
+                  'nonce': null,
+                },
+              ),
+            ],
+          );
+        },
+      );
+    },
+  );
+
+  test(
+    'endSession',
+    () async {
+      await flutterAppAuth.endSession(
+        EndSessionRequest(
+          idTokenHint: 'someIdToken',
+          postLogoutRedirectUrl: 'somePostLogoutRedirectUrl',
+          state: 'someState',
+          discoveryUrl: 'someDiscoveryUrl',
+        ),
+      );
+      expect(
+        log,
+        <Matcher>[
+          isMethodCall(
+            'endSession',
+            arguments: <String, Object?>{
+              'idTokenHint': 'someIdToken',
+              'postLogoutRedirectUrl': 'somePostLogoutRedirectUrl',
+              'state': 'someState',
+              'allowInsecureConnections': false,
+              'additionalParameters': null,
+              'issuer': null,
+              'discoveryUrl': 'someDiscoveryUrl',
+              'serviceConfiguration': null,
+              'preferEphemeralSession': false,
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
